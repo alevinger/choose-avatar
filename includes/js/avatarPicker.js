159f52645fbf991e03f81a6da69a40f325bbc31f
alevinger <ayala.levinger@gmail.com>
2018-09-25 09:50:25 +0200

    relocate onclick to inside the plugin

--- includes/js/avatarPicker.js
+++ includes/js/avatarPicker.js
@@ -2,27 +2,69 @@
 
     var AvatarPicker = function(element, options) {
         this.$element = $( element );
-        this.modal = this.$element.find('.modal');
+        //this.modal = this.$element.find('.modal');
         this.init( options );
         this.$element.data('avatarpicker', this);
+        this.curretAvatarId = null;
         return this;
     };
 
 
     AvatarPicker.prototype = {
         defaultOptions : {
-            avatarList: [],
-            currentAvatarId: null,
+            avatarList: [
+                {"src": "./includes/images/img6.png", "id": 1},
+                { "src": "./includes/images/img5.png", "id": 2 },
+                { "src": "./includes/images/img4.png", "id": 3 },
+                { "src": "./includes/images/img3.png", "id": 4 },
+                { "src": "./includes/images/img2.png", "id": 5 },
+                { "src": "./includes/images/img1.png", "id": 6 }
+            ],
+            placeholder: "",
+            defaultAvatarId: 1,
             previewContainer: 'avatarView',
             thumbnailsContainer: 'thumbnailsContainer',
-            test: 1
         },
         constructor: AvatarPicker,
         init: function (options) {
             var self = this;
             self.options = $.extend({}, this.defaultOptions, options);
-            self.thumbnailsContainer = $('#'+self.options.thumbnailsContainer);
+
+            self.curretAvatarId =  Cookies.get("currentAvatar") != undefined ? Cookies.get("currentAvatar") : self.options.defaultAvatarId;
+
+            self.createPreview();
+            self.createModal();
+        },
+
+        createPreview: function() {
+            var self = this;
+            var preview = $("<img id='avatarView' class='img-circle'/>");
+            self.$element.append(preview);
             self.previewContainer = $('#'+self.options.previewContainer);
+
+            var currentAvatarObj = self.options.avatarList.find( function (x) { return x.id == self.curretAvatarId } );
+            self.previewContainer.data("id", currentAvatarObj.id).attr("src", currentAvatarObj.src);
+        },
+
+        createModal: function() {
+            var self = this;
+
+            var modal = $(
+                "<div class='modal'  id='avatarsSpace' style='display: none;'>" +
+                "<div class='modal-dialog'><div class='modal-content'>" +
+                "<div class='modal-header'><div id='triangle'></div></div>" +
+                "<div class='modal-body'><p class='title'>"+self.options.placeholder+"</p>"+
+                "<div id='thumbnailsContainer'></div></div></div></div></div>"
+            );
+
+            self.$element.append(modal);
+            self.modal = self.$element.find('.modal');
+            self.thumbnailsContainer = self.modal.find('#'+self.options.thumbnailsContainer);
+            self.populateThumbnailsContainer();
+        },
+
+        populateThumbnailsContainer: function() {
+            var self = this;
 
             if(self.options.avatarList.length) {
                 var list = $( "<ul></ul>" );
@@ -37,6 +79,7 @@
             };
 
             self.setSelection(self.options.currentAvatarId);
+            self.bindPreview();
         },
 
         setSelection: function ( id ) {
@@ -48,6 +91,13 @@
                     $(this).removeClass('active').removeClass('loader');
                 }
             })
+        },
+
+        bindPreview: function() {
+            var self = this;
+            self.$element.find('#'+self.options.previewContainer).on('click', '', self, function (e) {
+                self.open();
+            });
         },
 
         bind: function (container, args) {
@@ -69,6 +119,7 @@
                 }
             });
         },
+
 
         open: function() {
             this.modal.show("slow");
